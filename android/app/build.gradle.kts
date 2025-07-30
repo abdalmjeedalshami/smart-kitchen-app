8fe838d637ccce0d13a90da1a2021fb90379efdd
Abd Al-Mjeed Al-Shami <ab.2267084@gmail.com>
2025-07-31 00:42:13 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 
 android {
     namespace = "com.example.meal_app_planner"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,14 +22,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/ap
         applicationId = "com.example.meal_app_planner"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 23
+        targetSdk= 34
+        versionCode =flutter.versionCode
+        versionName= flutter.versionName
     }
 
     buildTypes {
